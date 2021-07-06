ec17d1fb4a87a336d057250de6f9f6cf3fe2f0e7
jsapp21 <jameseisapp@gmail.com>
2021-07-06 12:49:55 -0700

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
+  config.active_storage.service = :google_dev
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -50,7 +51,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.active_storage.service = :google_dev
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
